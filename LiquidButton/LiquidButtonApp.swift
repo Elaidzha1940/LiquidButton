e021c027046da25c580dabeb20eaa617ef96a139
Elaidzha1940 <eli080293@gmail.com>
2023-09-15 00:22:06 +0300

    LiquidButton

---
+++ LiquidButton/LiquidButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidButtonApp.swift
+//  LiquidButton
+//
+//  Created by Elaidzha Shchukin on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LiquidButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
