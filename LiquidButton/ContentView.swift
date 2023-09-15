39d59bf50e5863f1086fd2a32b0d005a45b0487e
Elaidzha1940 <eli080293@gmail.com>
2023-09-15 14:05:33 +0300

    LiquidButton + added canvas + symbols

--- LiquidButton/ContentView.swift
+++ LiquidButton/ContentView.swift
@@ -18,14 +18,19 @@ struct ContentView: View {
             Rectangle()
                 .mask(canvas)
                 .overlay {
-                    ZStack {
-                        Circle()
-                            .frame(width: 60)
-                        Image(systemName: "plus")
-                            .font(.system(size: 30, weight: .bold))
-                            .foregroundColor(.gray)
+                    Button {
+                        show.toggle()
+                    } label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 60)
+                                .foregroundColor(.black)
+                            Image(systemName: "plus")
+                                .font(.system(size: 30, weight: .bold))
+                                .foregroundColor(.gray)
+                        }
+                        .offset(x: -20, y: -30)
                     }
-                    .offset(x: -20, y: -30)
                     .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .bottomTrailing)
                 }
         }
@@ -33,6 +38,8 @@ struct ContentView: View {
     
     var canvas: some View {
         Canvas { context, size in
+            context.addFilter(.alphaThreshold(min: 0.4))
+            context.addFilter(.blur(radius: 10))
             context.drawLayer { drawingContext in
                 let drawPoint = CGPoint(x: size.width - 50, y: size.height - 60)
                 for index in 1...2 {
@@ -46,7 +53,7 @@ struct ContentView: View {
                 .tag(1)
             Circle().frame(width: 60)
                 .tag(2)
-                .offset(y: show ? 50 : 0)
+                .offset(y: show ? -50 : 0)
 
         }
     }
