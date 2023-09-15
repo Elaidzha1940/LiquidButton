a46ce3125577392551bdae5cc7a9b6bb11fd690b
Elaidzha1940 <eli080293@gmail.com>
2023-09-15 13:54:54 +0300

    LiquidButton + added canvas

--- LiquidButton/ContentView.swift
+++ LiquidButton/ContentView.swift
@@ -1,21 +1,44 @@
+//  /*
 //
-//  ContentView.swift
-//  LiquidButton
+//  Project: LiquidButton
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.09.2023
 //
-//  Created by Elaidzha Shchukin on 15.09.2023.
+//  Status: in progress | Not decorated
 //
+//  */
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Rectangle()
+                .mask(canvas)
+                .overlay {
+                    Circle()
+                        .frame(width: 60)
+                    Image(systemName: "plus")
+                        .font(.system(size: 30, weight: .bold))
+                        .foregroundColor(.gray)
+                }
         }
-        .padding()
+    }
+    var canvas: some View {
+        Canvas { context, size in
+            context.drawLayer { drawingContext in
+                let drawPoint = CGPoint(x: size.width - 50, y: size.height - 60)
+                for index in 1...2 {
+                    if let symbol = context.resolveSymbol(id: index) {
+                        drawingContext.draw(symbol, at: drawPoint)
+                    }
+                }
+            }
+        } symbols: {
+            Circle().frame(width: 60)
+                .tag(1)
+        }
     }
 }
 
--- LiquidButton/LiquidButtonApp.swift
+++ LiquidButton/LiquidButtonApp.swift
@@ -1,9 +1,13 @@
+//  /*
 //
-//  LiquidButtonApp.swift
-//  LiquidButton
+//  Project: LiquidButton
+//  File: LiquidButtonApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 15.09.2023
 //
-//  Created by Elaidzha Shchukin on 15.09.2023.
+//  Status: in progress | Not decorated
 //
+//  */
 
 import SwiftUI
 
